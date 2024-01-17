758c654a9dfbea0e03db092d5f6efd425ac6fc56
theboyGolden <preciousstick15@gmail.com>
2024-01-17 20:47:27 +0000

    task 1 tackled

--- task1.java
+++ task1.java
@@ -5,7 +5,7 @@
 // getColor() method that returns the color of the shape.
 // isFilled() method that returns whether the shape is filled or not.
 // setColor(String color) method that sets the color of the shape.
-// setFilled(boolean filled) method that sets whether the shape is filled or not.
+// setFilled(boolean_filled) method that sets whether the shape is filled or not.
 //
 // Create two subclasses, Circle and Rectangle, that inherit from the Shape class.
 //
@@ -24,3 +24,67 @@
 // Override the toString() method to provide a meaningful string representation of the rectangle.
 //
 // Create instances of both Circle and Rectangle classes, set their attributes, and display their information using the toString() method.
+
+
+class Shape {
+    String color;
+    boolean filled;
+
+    String getColor() {
+        return this.color;
+    }
+
+    boolean isFilled() {
+        return this.filled;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+
+    void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+}
+
+class Circle extends Shape {
+    double radius;
+
+    Circle() {
+        this.radius = 0;
+        this.color = "New";
+        this.filled = false;
+    }
+
+    double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    //Math.PI is the java representation of a pi which is approximately 3.142. So therefore the Area becomes pi * radius squared
+
+    @Override
+    public String toString() {
+        return "Circle [radius=" + radius + ", color=" + color + ", filled=" + filled + "]";
+    }
+}
+
+class Rectangle extends Shape {
+    double width;
+    double length;
+
+    Rectangle() {
+        this.width = 0;
+        this.length = 0;
+        this.color = "New";
+        this.filled = false;
+    }
+
+    double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle [width=" + width + ", length=" + length + ", color=" + color + ", filled=" + filled + "]";
+    }
+}
