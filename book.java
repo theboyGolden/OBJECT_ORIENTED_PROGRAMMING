8b2c6190ce56869ecc1ce249b3e5da7f0c0589c2
theboyGolden <preciousstick15@gmail.com>
2024-01-18 22:05:30 +0000

    renamed task files to properly implement the java files

---
+++ book.java
@@ -0,0 +1,15 @@
+// Create a class called Book with the following attributes and methods:
+
+// Attributes: title (String), author (String), isbn (String), numPages (int).
+// Methods:
+// displayInfo() method that prints out the information about the book, including title, author, ISBN, and number of pages.
+// Implement a constructor for the Book class to initialize the attributes.
+
+// Create an instance of the Book class, set some initial values, and call the displayInfo() method to print out the book details.
+
+
+
+
+public class book {
+    
+}
