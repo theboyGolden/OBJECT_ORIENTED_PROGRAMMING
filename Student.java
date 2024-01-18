0b9c65a965c69f532af75a5efbcdbb387f2734eb
theboyGolden <preciousstick15@gmail.com>
2024-01-18 22:39:56 +0000

    Student added

---
+++ Student.java
@@ -0,0 +1,11 @@
+// Create a class called Student with the following attributes and methods:
+
+// Attributes: name (String), id (String), grade (double).
+// Methods:
+// displayInfo() method that prints out the information about the student, including name, ID, and grade.
+// Implement a constructor for the Student class to initialize the attributes.
+// Create an instance of the Student class, set some initial values, and call the displayInfo() method to print out the student details.
+
+
+
+
