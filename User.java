f34b74b73cd774f725e6ec8723c2c99260c32095
theboyGolden <preciousstick15@gmail.com>
2024-01-19 16:50:01 +0000

    Forgot to save changes

--- User.java
+++ User.java
@@ -26,16 +26,80 @@
 
 
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class User {
     private String userId;
     private String username;
     private String email;
-    private friends <String> ; 
+    private List<User> friends; 
 
-    User () {
-        this.userId = userId;
+    // Constructor
+    public User(String username, String email) {
         this.username = username;
         this.email = email;
-        this.friends = new ArrayList<User>();
+        this.friends = new ArrayList<>(); // Initialize the list
+    }
+
+
+    public void addFriend(User friend) {
+        friends.add(friend);
+    }
+
+    public void removeFriend(User friend) {
+        friends.remove(friend);
+    }
+
+    public List<User> getFriends() {
+        return friends;
+    }
+
+    public class Post {
+        private String postId;
+        private String content; 
+        private User author; 
+        private Date timestamp;
+
+        // Constructor for Post
+        public Post(String content, User author, Date timestamp) {
+            this.content = content;
+            this.author = author;
+            this.timestamp = timestamp;
+        }
+
+        public void displayInfo() {
+            System.out.println("Post ID: " + postId);
+            System.out.println("Content: " + content);
+            System.out.println("Author: " + author.getUsername()); // Access the username through getter
+            System.out.println("Timestamp: " + timestamp);
+        }
+    }
+
+    public void displayInfo() {
+        System.out.println("User Id: " + userId);
+        System.out.println("User Name: " + username);
+        System.out.println("Email: " + email);
+
+        // Display friends
+        System.out.println("Friends:");
+        for (User friend : friends) {
+            System.out.println("- " + friend.getUsername());
+        }
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Golden Boy", "theboygolden@gmail.com");
+
+        user1.displayInfo();
+    }
+
+    // Getter for username
+    public String getUsername() {
+        return username;
     }
 }
+
+
+
