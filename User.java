d1a4eb1d142db413b34f343edf291c33e836eea9
theboyGolden <preciousstick15@gmail.com>
2024-01-19 15:53:01 +0000

    Social Media System task added

---
+++ User.java
@@ -0,0 +1,31 @@
+// Create a class called User with the following attributes:
+
+// userId (String)
+// username (String)
+// email (String)
+// friends (List of User) - to store a list of friends.
+// Implement a constructor to initialize the attributes when a User object is created.
+
+// Create methods in the User class for the following operations:
+
+// addFriend(User friend) - to add a user as a friend.
+// removeFriend(User friend) - to remove a user from the friend list.
+// getFriends() - to retrieve the list of friends.
+// Create a class called Post with the following attributes:
+
+// postId (String)
+// content (String)
+// author (User)
+// timestamp (Date or String)
+// Implement a constructor to initialize the attributes when a Post object is created.
+
+// Create a method in the Post class to display information about the post, including the author and timestamp.
+
+// In the main method, create instances of the User and Post classes, perform operations like adding/removing friends, and create posts with comments.
+
+
+
+
+public class User {
+    
+}
